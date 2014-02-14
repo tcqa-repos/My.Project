4b2bf9dfcc7ff46d776c02a3e7fc60b699b78ab0
Test &lt <textarea>
2014-02-14 19:17:32 +0400

    write to file

---
+++ write_to_file.ps1
@@ -0,0 +1,17 @@
+$text = 'Hello World'
+
+# Create file:
+
+$text | Set-Content 'file.txt'
+#or
+$text | Out-File 'file2.txt'
+#or
+$text > 'file3.txt'
+
+# Append to file:
+
+$text | Add-Content 'file.txt'
+#or
+$text | Out-File 'file2.txt' -Append
+#or
+$text >> 'file3.txt'
