4f93ed0515511dca01ced3afaf86bca82daaee5d
Repository with test projects <teamcity.qa.server@gmail.com>
2017-03-15 23:15:31 +0300

    Update Powershell_Arguments_PowerShell20.kt

--- .teamcity/Powershell_Arguments/buildTypes/Powershell_Arguments_PowerShell20.kt
+++ .teamcity/Powershell_Arguments/buildTypes/Powershell_Arguments_PowerShell20.kt
@@ -8,6 +8,18 @@ object Powershell_Arguments_PowerShell20 : BuildType({
     extId = "Powershell_Arguments_PowerShell20"
     name = "PowerShell 2.0"
     paused = true
+    
+    steps {
+        powerShell {
+            name = "One argument with space"
+            id = "RUNNER_55_B"
+            scriptMode = file {
+                path = "argument.ps1"
+            }
+            noProfile = false
+            param("jetbrains_powershell_scriptArguments", """"one test-message"""")
+        }
+    }
 
     requirements {
         equals("powershell_x86", "2.0", "RQ_28")
